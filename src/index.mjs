e880bab74fe1dd9842b7b1077116c63fdc98359d
Ali Elouafiq <ali@beyond-believers.com>
2022-07-23 13:39:45 +0100

    properly exporting the module

--- src/index.mjs
+++ src/index.mjs
@@ -3,10 +3,12 @@ import Order from "./order.mjs";
 import InvoiceFile from "./invoice-file.mjs";
 import APISession from "./api-session.mjs";
 import ExportOrdersFetcher from "./export.mjs";
-export default {
+import * as Helpers from './helpers.mjs'
+export {
     APISession,
     OrderQueueBuilder,
     Order,
     InvoiceFile,
-    ExportOrdersFetcher
+    ExportOrdersFetcher,
+    Helpers
 }
